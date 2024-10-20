7e68d78e4a0f28cabec0a2ee3f9d2f4614f3d87a
anjanetttee <anjanette.auwlia@gmail.com>
2024-10-19 17:33:52 -0700

    faq page updated

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,7 @@
 
+function goToNextPage(){
+    window.location.href = 'start.html';
+}
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
@@ -11,3 +14,19 @@ const observer = new IntersectionObserver((entries)=>{
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el));
+
+
+const faqHeader = document.querySelectorAll(".faq-header");
+faqHeader.forEach(faqHeader=>{
+    faqHeader.addEventListener("click", event =>{
+        faqHeader.classList.toggle("active");
+        const faqBody = faqHeader.nextElementSibling;
+        if(faqHeader.classList.contains("active")){
+            faqBody.style.maxHeight = faqBody.scrollHeight + "px";
+        }else{
+            faqBody.style.maxHeight = 0;
+        }
+    });
+    
+});
+
